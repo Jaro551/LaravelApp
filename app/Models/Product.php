0f281809c25d4058d44c3926180c9714e0673ba3
Jaro551 <jaro2362@gmail.com>
2021-07-26 15:04:03 +0200

    Seeder for products

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /** 
+    * @var string
+    */
+    protected $table = "products";
+
+    /** 
+    * @var bool
+    */
+    public $timestamps = false;
+
+    /** 
+    * @var array
+    */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}
